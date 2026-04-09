ae5f56c46851d656ee19958758063afba00b0966
Nasir Mohammad <erfan.nasir@gmail.com>
2026-04-09 07:46:11 +0000

    imrove deploy logs

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+
+echo "=== DEPLOY START ==="
+
 cd /root/mysite || exit
+
+echo "Pulling latest code..."
 git pull origin main
+
+echo "Stopping containers..."
 sudo docker-compose down
+
+echo "Starting containers..."
 sudo docker-compose up -d --build
+
+echo "=== DEPLOY END ==="
