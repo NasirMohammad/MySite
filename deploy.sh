5cc7fb85038b699fe8c0aa06506e32a4b4a1a922
Nasir Mohammad <erfan.nasir@gmail.com>
2026-04-09 09:01:25 +0000

    enable zero downtime deploy

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,7 @@ cd /root/mysite || exit
 echo "Pulling latest code..."
 git pull origin main
 
-echo "Stopping containers..."
-sudo docker-compose down
-
-echo "Starting containers..."
+echo "Rebuilding containers (zero downtime)..."
 sudo docker-compose up -d --build
 
 echo "=== DEPLOY END ==="
